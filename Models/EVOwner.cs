e0b2a96eeb06dd5539f8896dcaaefb7b093803c1
Maneth Dewpura <dewpuramaneth@gmail.com>
2025-09-26 11:44:58 +0530

    User profile controllers

---
+++ Models/EVOwner.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace SparkPoint_Server.Models
+{
+    public class EVOwner
+    {
+        [BsonId]
+        public string NIC { get; set; }
+
+        [BsonElement("phone")]
+        public string Phone { get; set; }
+
+        [BsonElement("userId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+
+        [BsonElement("createdAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("updatedAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+
+}
