2eaabbe7006edd5cb4cce8b041a3f29afd5709bb
Maneth Dewpura <dewpuramaneth@gmail.com>
2025-09-26 12:16:18 +0530

    Refactor: Remove unused user and owner model classes; add EVOwner model and related classes

---
+++ Models/EVOwnerModels.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace SparkPoint_Server.Models
+{
+    public class EVOwner
+    {
+        [BsonId]
+        public string NIC { get; set; }
+
+        [BsonElement("phone")]
+        public string Phone { get; set; }
+
+        [BsonElement("userId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+
+        [BsonElement("createdAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("updatedAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+    public class EVOwnerRegisterModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string NIC { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class EVOwnerUpdateModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
