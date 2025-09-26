b298e582070d7c957d0ae3bb537866443c4499fb
Maneth Dewpura <dewpuramaneth@gmail.com>
2025-09-26 11:47:38 +0530

    main configurations

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace SparkPoint_Server
 {
@@ -9,16 +14,76 @@ namespace SparkPoint_Server
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            // JSON Serialization Settings
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            config.Formatters.JsonFormatter.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
 
-            // Web API routes
+            // Remove XML formatter to return only JSON
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Add custom message handler for CORS
+            config.MessageHandlers.Add(new CorsMessageHandler());
+
+            // Web API routes - Attribute routing first (enables [Route] attributes)
             config.MapHttpAttributeRoutes();
 
+            // Convention-based routes for backward compatibility
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Additional route for actions
+            config.Routes.MapHttpRoute(
+                name: "ActionApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Enable detailed error information (disable in production)
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+        }
+    }
+
+    // Custom CORS message handler
+    public class CorsMessageHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            // Handle preflight OPTIONS requests
+            if (request.Method == HttpMethod.Options)
+            {
+                var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+                AddCorsHeaders(response);
+
+                var tcs = new TaskCompletionSource<HttpResponseMessage>();
+                tcs.SetResult(response);
+                return tcs.Task;
+            }
+
+            // Continue with the request and add CORS headers to the response
+            return base.SendAsync(request, cancellationToken).ContinueWith(task =>
+            {
+                var response = task.Result;
+                AddCorsHeaders(response);
+                return response;
+            }, cancellationToken);
+        }
+
+        private void AddCorsHeaders(HttpResponseMessage response)
+        {
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+            response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            response.Headers.Add("Access-Control-Max-Age", "86400"); // 24 hours
         }
     }
 }
---
+++ Helpers/MongoDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using System.Configuration;
+
+namespace SparkPoint_Server.Helpers
+{
+
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext()
+        {
+            var connectionString = ConfigurationManager.AppSettings["MongoDbConnection"];
+            var databaseName = ConfigurationManager.AppSettings["MongoDbDatabase"];
+
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _database.GetCollection<T>(name);
+        }
+    }
+
+}
